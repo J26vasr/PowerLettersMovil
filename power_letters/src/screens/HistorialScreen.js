c331b582fa98f6fa55f00b6182bee635df73aece
JV26vasr <veronicaro3101qr@gmail.com>
2024-07-03 21:08:17 -0600

    realizando cambios actualizados del historial

--- power_letters/src/screens/HistorialScreen.js
+++ power_letters/src/screens/HistorialScreen.js
@@ -9,26 +9,30 @@ const HistorialScreen = () => {
 
   const products = [
     {
-      title: 'Yo antes de ti',
-      precio: 'Precio: $14.99',
+      title: '3/01/21',
+      libro:'Titulo: Cruce de caminos',
+      precio: 'Total: $14.99',
       image: 'https://marketplace.canva.com/EAFutLMZJKs/1/0/1003w/canva-portada-libro-novela-suspenso-elegante-negro-wxuYB_sJtMw.jpg',
       cantidad: 'Cantidad: 3',
     },
     {
-      title: 'Yo antes de ti',
-      precio: 'Precio: $14.99',
+      title: '14/03/23',
+      libro:'Titulo: Cruce de caminos',
+      precio: 'Total: $14.99',
       image: 'https://marketplace.canva.com/EAFutLMZJKs/1/0/1003w/canva-portada-libro-novela-suspenso-elegante-negro-wxuYB_sJtMw.jpg',
       cantidad: 'Cantidad: 3',
     },
     {
-      title: 'Yo antes de ti',
-      precio: 'Precio: $14.99',
+      title: '07/04/22',
+      libro:'Titulo: Cruce de caminos',
+      precio: 'Total: $14.99',
       image: 'https://marketplace.canva.com/EAFutLMZJKs/1/0/1003w/canva-portada-libro-novela-suspenso-elegante-negro-wxuYB_sJtMw.jpg',
       cantidad: 'Cantidad: 3',
     },
     {
-      title: 'Yo antes de ti',
-      precio: 'Precio: $14.99',
+      title: '23/05/24',
+      libro:'Titulo: Cruce de caminos',
+      precio: 'Total: $14.99',
       image: 'https://marketplace.canva.com/EAFutLMZJKs/1/0/1003w/canva-portada-libro-novela-suspenso-elegante-negro-wxuYB_sJtMw.jpg',
       cantidad: 'Cantidad: 3',
     },
@@ -52,13 +56,8 @@ const HistorialScreen = () => {
             <Text style={styles.cardTitle}>{product.title}</Text>
             <Text style={styles.cardDescription}>{product.precio}</Text>
             <Text style={styles.cardCantidad}>{product.cantidad}</Text>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => navigation.navigate('DetallesProducto', { producto: product })}
-            >
-              <Text style={styles.buttonText}>Ver más</Text>
-            </TouchableOpacity>
-
+            <Text style={styles.cardLibro}>{product.libro}</Text>
+            
           </View>
         ))}
       </View>
@@ -121,11 +120,11 @@ const styles = StyleSheet.create({
     marginRight: 230,
     width: 120,
     height: 160,
-    borderRadius: 20,
+    borderRadius: 30,
     marginBottom: 15,
   },
   cardTitle: {
-    marginTop:  -160,
+    marginTop:  -140,
     marginLeft: 90,
     fontSize: 20,
     fontWeight: 'bold',
@@ -143,6 +142,12 @@ const styles = StyleSheet.create({
   cardCantidad:{
     marginLeft: 50,
     marginTop: -10,
+    color: '#555',
+
+  },
+  cardLibro:{
+    marginLeft: 130,
+    marginTop: 5,
     color: '#555',
 
   },
